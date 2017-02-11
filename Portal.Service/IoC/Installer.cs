3b0064d2cb30a812a15607115b0fd8fd0cf9d622
madhavabhyankar <madhavsemail@gmail.com>
2017-02-11 16:25:25 -0600

    Portal Shell

---
+++ Portal.Service/IoC/Installer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System.Web.Http;
+
+namespace Portal.Service.IoC
+{
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Classes.FromThisAssembly().BasedOn<ApiController>().LifestyleTransient());
+        }
+    }
+}
---
+++ Portal.Service/OwinStartup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Newtonsoft.Json;
+using Owin;
+using WindsorWebApiDependency;
+
+namespace Portal.Service
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            var config = new HttpConfiguration();
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.DependencyResolver = new WindsorDependencyResolver(PortalServiceRunnable.Container);
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            app.UseWebApi(config);
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Path.Value.Contains(".")
+                    && !context.Request.Path.Value.Contains("api"))
+                {
+                    context.Request.Path = new PathString("/index.html");
+                }
+                await next();
+            });
+
+            app.UseFileServer(new FileServerOptions
+            {
+                FileSystem = new EmbeddedResourceFileSystem(GetType().Assembly, "Temple.Service.Build"),
+                EnableDirectoryBrowsing = false,
+                EnableDefaultFiles = true,
+                RequestPath = new PathString("")
+            });
+            var requiredScopes = ConfigurationManager.AppSettings["RequiredScopes"];
+            var scopes = requiredScopes.Contains(",")
+                ? requiredScopes.Split(new[] { ',' }).ToList()
+                : new List<string> { requiredScopes };
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = ConfigurationManager.AppSettings["IdentityUrl"],
+                RequiredScopes = scopes
+            });
+
+
+
+        }
+    }
+}
